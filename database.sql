5df8a1fe9658e8a5bc15679654b2c467392f7d92
Matt Dow <matt.dow.vo@gmail.com>
2021-10-15 16:57:16 -0500

    Added get method

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "tasks" (
 	"id" SERIAL PRIMARY KEY,
 	"taskname" VARCHAR (255) NOT NULL,
-	"priority" VARCHAR (12),
+	"priority" INTEGER,
 	"dueDate" DATE,
 	"contextTag" VARCHAR (50),
 	"completeStatus" BOOLEAN
@@ -10,6 +10,7 @@ CREATE TABLE "tasks" (
 INSERT INTO "tasks"
 	("taskname", "priority", "dueDate", "contextTag", "completeStatus")
 VALUES
-	('Oil Change', 'high', '10-21-2021', 'errand', false),
-	('LinkedIn post', 'medium', '10-17-2021', 'office', false),
-	('Touch base with Paul', 'low', '11-01-2021', 'home', false); 
+	('Oil Change', 1, '10-21-2021', 'errand', false),
+	('LinkedIn post', 2, '10-17-2021', 'office', false),
+	('Touch base with Paul', 3, '11-01-2021', 'home', false); 
+	
--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -3,8 +3,19 @@ const router = express.Router();
 
 const pool = require('../modules/pool');
 
+// get request for all tasks
 
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "tasks" ORDER BY "completeStatus", "priority", "dueDate";';
+    pool.query(queryText).then(result => {
 
+        res.send(result.rows);
+    })
+    .catch(error => {
+        console.log('error getting tasks from server and db', error);
+        res.sendStatus(500);
+    }); // end of catch statement
+}); // end of get method
 
 
 
