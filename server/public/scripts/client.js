f848ead307f336d8b73a052d39cb021a6a03ce1c
Matt Dow <matt.dow.vo@gmail.com>
2021-10-15 21:14:47 -0500

    Added basic get and rendering code

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,7 +2,51 @@ console.log('JS active');
 
 $(document).ready(function () {
 console.log('jQuery sourced.');
-
+// initial calling of task list
+refreshTasks();
 
 
 });
+
+// refreshTasks will get all tasks currently in server/DB and render to page
+function refreshTasks() {
+    $.ajax({
+        type: 'GET',
+        url: '/tasks'
+    }).then(function(response) {
+        console.log('Tasklist refreshing', response);
+        renderTasks(response);        
+    }).catch(function(error){
+        console.log('error in GET', error);        
+    });
+}
+
+// renderBooks will display the current array of tasks to the DOM
+function renderTasks(tasks) {
+  // empty the active task table
+    $('#taskTable').empty();
+    // loop through the tasks
+    for(let i=0; i < tasks.length; i++) {
+        let task = tasks[i];
+        // set a temp variable equating to the completed status
+        let completed = 'No';
+        if (task.completeStatus) {
+            completed = 'Yes';
+        }
+        // For each book, append a new row to the table
+        let taskRow = $(`
+        <tr data-id="${task.id}">
+            <td>${task.taskname}</td>
+            <td>${task.priority}</td>
+            <td>${task.dueDate}</td>
+            <td>${task.contextTag}</td>
+            <td>
+                <button class="completeBtn">Mark Complete</button>
+            </td>
+            <td>
+                <button class="deleteBtn">Delete Task</button>
+            </td>
+            `) // end of row rendering code
+            $('#taskTable').append(taskRow);
+    } // end of for loop
+} // end of renderTasks
